b0b8cf958dcaa564ea594faf01793dbc0a4a707d
Rafi <rafikulswe.com>
2019-01-12 01:54:03 +0600

    array discussion

---
+++ array.js
@@ -0,0 +1,14 @@
+var name = ['Rafi', 'Mafi', 'Kafi', 'Safi'];
+
+console.log(name);
+console.log(name[1]);
+console.log(name.length);
+
+
+var age = {
+    Rafi : 24,
+    Safi : 22,
+    Kafi : 20
+};
+console.log(age['Rafi']);
+console.log(age);
