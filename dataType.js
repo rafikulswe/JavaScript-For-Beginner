4c82bde14737ac839288cdc22fb225bf27a252e4
Rafi <rafikulswe.com>
2019-01-11 16:21:32 +0600

    javascript practice

---
+++ dataType.js
@@ -0,0 +1,17 @@
+//DataType = Number, string, Boolean, Undefind, null, Array, Object, Function.
+var name = "Mr Rafi";
+var num = 10;
+var nameArr = ['Rafi', 'Rafikul', 'Miske', 'Moni'];
+var something;  //undefind
+var anything = null;
+
+console.log(typeof name);
+console.log(typeof num);
+console.log(typeof nameArr);
+console.log(typeof something);
+
+console.log(something == anything);
+console.log(undefined == null);
+console.log(something === anything);
+
+
