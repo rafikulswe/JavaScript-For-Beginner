cc0885346fd6c7fd7fb1d670e62fd7b540d7fc2e
Rafi <rafikulswe.com>
2019-01-13 01:28:12 +0600

    else if added

--- if-else.js
+++ if-else.js
@@ -1,7 +1,9 @@
 var age = 24;
 
-if(age = 24){
+if(age = 18){
     console.log("You are eligible for vote");
+}else if(age = 24){
+    console.log('I are adult');
 }else{
     console.log('I are not');
 }
