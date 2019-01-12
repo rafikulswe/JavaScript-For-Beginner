a2c92422f8632b23879d73df6b9c1860eaf08142
Rafi <rafikulswe.com>
2019-01-13 00:47:27 +0600

    if else codition details

---
+++ if-else.js
@@ -0,0 +1,15 @@
+var age = 24;
+
+if(age = 24){
+    console.log("You are eligible for vote");
+}else{
+    console.log('I are not');
+}
+
+//Another example
+// === means it check the data value + data type
+if(age === '24'){
+    console.log("You are eligible for vote");
+}else{
+    console.log('Your value is a string. Need number');
+}
