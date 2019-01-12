5ba90361729a6ae6a47d60cfeb1d101b55da5154
Rafi <rafikulswe.com>
2019-01-13 01:46:04 +0600

    for loop

---
+++ for-loop.js
@@ -0,0 +1,15 @@
+//simple for loop example
+var name = "Md.Rafikul Islam";
+for(var i=0; i<=10; i++){
+    console.log(name);
+}
+
+//Another for loop example
+var nameArray = ['Rafi', 'Moni', 'Rafiya'];
+var text = '';
+for(var i = 0; i < nameArray.length; i++){
+    text += nameArray[i] + '<br>';
+}
+console.log(text);
+
+
