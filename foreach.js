e4de73e752898ec29563cceb467748385cc27788
rafiswe <rafikulislam521@gmail.com>
2019-01-14 01:19:23 +0600

    foreach file updated.

--- foreach.js
+++ foreach.js
@@ -4,3 +4,15 @@ amountArr.forEach(arrayItem => {
     result += arrayItem;
 });
 console.log('Total Amount is '+result);
+
+
+//How foreach function work
+var names = ['Mr. Rafi', 'Mr. Messi', 'Ms Miske'];
+function arrTraverse(names, rollback){
+    for(var i=0; i<=names.length; i++){
+        rollback(names[i]);
+    }
+}
+arrTraverse(names, function(newPara){
+    console.log(newPara);
+});
