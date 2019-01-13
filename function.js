279c3ffe351b9e032ec96199e88370e46b24f59f
rafiswe <rafikulislam521@gmail.com>
2019-01-14 00:24:04 +0600

    function and callback function details

--- function.js
+++ function.js
@@ -1,4 +1,37 @@
+//Simple example of function
 function valueSum(a,b){
     return a + b;
 }
-console.log(valueSum(10,20));
+console.log('Sum of these value is '+valueSum(10,20));
+
+//we can store a function in a variable
+var sumResult = valueSum(15, 20);
+console.log(sumResult);
+
+//CallBack Function
+var names = ['Mr. Rafi', 'Mr. Messi', 'Ms Miske'];
+names.forEach(print);
+
+function print(nameParam){
+    console.log(nameParam);  
+}
+
+//we can use this function as a parameter inline.
+names.forEach(function(nameParam){
+    console.log(nameParam);
+})
+
+//Another dynamic Callback function example
+function operation(a, b, CallBack){
+    var sum = a+b;
+    var sub = a-b;
+    CallBack(sum, sub);
+}
+function Output(sum, sub){
+    console.log(sum, sub);
+}
+function multiply(sum, sub){
+    console.log(sum*sub);
+}
+operation(10, 5, Output);
+operation(10, 5, multiply);
