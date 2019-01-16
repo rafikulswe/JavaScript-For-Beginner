fe49e8a7a1d6a1472faf9d92e1418ae96a723477
rafiswe <rafikulislam521@gmail.com>
2019-01-16 23:54:05 +0600

    updated

--- array-method.js
+++ array-method.js
@@ -1,2 +1,13 @@
 //Array Method- pop(), push(), 
+pop()
+var fruits = ['apple', 'banana0', 'mango'];
+console.log(fruits.pop());
+console.log(fruits);
+
+//push()
+fruits.push('lemon');
+console.log(fruits);
+
+
+
 
