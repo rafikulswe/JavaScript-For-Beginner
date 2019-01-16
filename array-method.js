e0dd24fa7c62750bea5803f7aaaf9df6d2ab2e15
rafiswe <rafikulislam521@gmail.com>
2019-01-16 23:58:53 +0600

    updated

--- array-method.js
+++ array-method.js
@@ -1,5 +1,5 @@
-//Array Method- pop(), push(), 
-pop()
+//Array Method- pop(), push(), shift(), unshift()
+//pop()
 var fruits = ['apple', 'banana0', 'mango'];
 console.log(fruits.pop());
 console.log(fruits);
@@ -7,6 +7,16 @@ console.log(fruits);
 //push()
 fruits.push('lemon');
 console.log(fruits);
+
+//shift()
+fruits.shift();
+console.log(fruits);
+
+//unshift()
+fruits.unshift('black bary');
+console.log(fruits);
+
+
 
 
 
