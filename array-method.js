09b391ca91aad9b676e734405057636cd7ec2dd6
rafiswe <rafikulislam521@gmail.com>
2019-01-18 01:20:13 +0600

    array details updated

--- array-method.js
+++ array-method.js
@@ -1,4 +1,4 @@
-//Array Method- pop(), push(), shift(), unshift()
+//Array Method- pop(), push(), shift(), unshift(), concat(), slice(), toString()
 //pop()
 var fruits = ['apple', 'banana0', 'mango'];
 console.log(fruits.pop());
@@ -19,6 +19,22 @@ console.log(fruits);
 //for change the index element of array
 fruits[0] = 'bary';
 console.log(fruits);
+
+//concat()
+var myGirls = ["Cecilie", "Lone"];
+var myBoys = ["Emil", "Tobias", "Linus"];
+var arr3 = ["Robin", "Morgan"];
+console.log(myGirls.concat(myBoys));
+console.log(myGirls.concat(myBoys, arr3));
+
+//slice()
+var fruits = ['apple', 'banana', 'mango', 'lemon'];
+console.log(fruits.slice(2)); //parameter is a array starting index.
+
+//toString()
+console.log(fruits.toString()); //it make an array to string.
+
+
 
 
 
