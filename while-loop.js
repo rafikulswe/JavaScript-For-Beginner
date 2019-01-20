572c2d979612c944db816920ebd3cd63d71a904e
rafiswe <rafikulislam521@gmail.com>
2019-01-20 23:00:37 +0600

    while loop example

---
+++ while-loop.js
@@ -0,0 +1,17 @@
+var i = 0;
+var text ="";
+while (i <= 10) {
+    text += ' number is'+' '+ i;
+    i++;
+}
+console.log(text);
+
+var cars = ["BMW", "Volvo", "Saab", "Ford"];
+var i = 0;
+var result = "";
+while (cars[i]) {
+    result += cars[i] + " ";
+  i++;
+}
+console.log(result);
+
