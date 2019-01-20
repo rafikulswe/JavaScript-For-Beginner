ffdf1275075c682fb15e25c51ab4dd1efb8c872a
rafiswe <rafikulislam521@gmail.com>
2019-01-20 22:42:09 +0600

    switch case example

---
+++ switch-condition.js
@@ -0,0 +1,12 @@
+var caseValue = 10;
+switch (caseValue) {
+    case 5:
+        console.log('very little boy');
+        break;
+    case 10:
+        console.log('big man');
+        break;
+    default:
+        console.log('None of these');
+        break;
+}
